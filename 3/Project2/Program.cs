871c96fb159fc993c2b10612d04d87a3eaa37808
Filip <39651273+Kotfil@users.noreply.github.com>
2019-02-01 23:41:02 +0200

    'DataType'

---
+++ 3/Project2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var index = 5; // int
+
+            var apple = "Apple"; // string
+
+
+            object ibj = 2;
+
+            object obj2 = "hello world!";
+
+
+
+            char a = 'A';
+            string hello = "Hello";
+            decimal rate = 5.4M;
+            double weight = 180.34;
+
+            float height = 166.23F;
+
+            byte b = 3;
+
+            bool isAlive = true;
+            isAlive = false;
+            int tax = 9
+            tax = 5;
+            Console.WriteLine(tax);
+            tax = 7;
+            Console.ReadKey();
+        }
+    }
+}
