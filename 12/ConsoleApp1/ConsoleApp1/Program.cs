49962d7f805152badb2e27e6a8b36e71789cca12
Filip <39651273+Kotfil@users.noreply.github.com>
2019-02-06 16:39:43 +0200

    study

---
+++ 12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 5;
+            int d = 6;
+            Console.WriteLine($"Initial value z = {z}");
+            AditionVal(z, d);
+        }
+        static void AdditionRef(ref int x, int y)
+        {
+            x = x + y;
+        }
+            static void AdditionVal(int x, int y)
+        {
+            x = x + y;
+        }
+        static string Hello()
+        }
+    return "hello world!";
+    }
+}
