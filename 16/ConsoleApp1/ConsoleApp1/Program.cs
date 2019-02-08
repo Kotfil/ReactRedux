94c2eba02a9ca1d69f0abef0b8705521169ecbcd
Filip <39651273+Kotfil@users.noreply.github.com>
2019-02-08 14:54:56 +0200

    study

--- 16/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,14 +8,17 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static (string,int) GetValue((string,int) tuple,int x)
+        {
+            var result = (name: tuple.Item1, age: tuple.Item2 + x);
+            return result;
+        }
         static void Main(string[] args)
         {
-            var tuple = (count:5, sum:10);
-            Console.WriteLine(tuple.count);
-            Console.WriteLine(tuple.sum);
+            var tuple = GetValue("Tom, 25), 10);
 
-            tuple.Item1 += 20;
             Console.WriteLine(tuple.Item1);
+            Console.WriteLine(tuple.Item2);
 
             Console.ReadKey();
         }
---
+++ 17/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ceb455bc-4d59-49f7-b7e9-219dd565df3b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
