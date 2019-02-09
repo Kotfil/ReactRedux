41e34f99ed4edbb713e444e9c62a765d6e404e55
Filip <39651273+Kotfil@users.noreply.github.com>
2019-02-09 09:29:27 +0200

    study

---
+++ 18/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User tom = new User("Tom", 23);
+            // tom.name = "Tom";
+            //tom.age = 22;
+            tom.Info();
+
+
+            User bob = new User("Bob") { age = 26 };
+            // tom.name = "Bob";
+            //tom.age = 22;
+            bob.Info();
+
+
+            Console.ReadKey();
+        }
+    }
+}
+    
