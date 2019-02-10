cf6037f249a7c5206d31d29e21419f956e53fcd3
Filip <39651273+Kotfil@users.noreply.github.com>
2019-02-10 14:32:11 +0200

    study

---
+++ 19/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate(5);
+            Console.ReadKey();
+        }
+        static void Calculate (int t )
+        {
+            int x = 6;
+            int y = 7;
+            int z = y + t;
+
+
+
+        }
+
+    }
+}
