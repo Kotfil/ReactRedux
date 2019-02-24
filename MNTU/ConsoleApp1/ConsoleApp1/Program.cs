4a662cb617ad11e05d88d8f753ba8f3c694ff46b
Filip <39651273+Kotfil@users.noreply.github.com>
2019-02-24 15:30:21 +0200

    study

---
+++ MNTU/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //method Parse
+            Console.WriteLine("Введіть ціле");
+            string strdata = Console.ReadLine();
+            int intdata = int.Parse(strdata);
+            Console.WriteLine("Введіть число з дробовою частиною і порядком");
+            strdata = Console.ReadLine();
+            double doubdata = double.Parse(strdata);
+            Console.WriteLine("intdata = {0}; doubdata = {1}",
+               intdata, doubdata);
+            Console.ReadKey();
+
+        }
+    }
+}
